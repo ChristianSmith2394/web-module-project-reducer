e6ba10a4feb99fff569e2c0f8f4d62486222c624
Christian <christiansmith2394@gmail.com>
2021-11-02 16:37:07 -0400

    MVP met

--- src/App.js
+++ src/App.js
@@ -1,73 +1,93 @@
-import React, { useReducer } from 'react';
-import { initialState } from './reducers';
-import reducer from './reducers';
+import React, { useReducer } from "react";
+import { initialState } from "./reducers";
+import reducer from "./reducers";
 
-import './App.css';
+import { applyNumber, changeOperation, clearDisplay, Memory } from "./actions";
 
-import TotalDisplay from './components/TotalDisplay';
-import CalcButton from './components/CalcButton';
+import "./App.css";
+
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-// function App() {
+  const handleMath = (value) => {
+    dispatch(applyNumber(value));
+  };
 
+  const handleOperator = (operator) => {
+    dispatch(changeOperation(operator));
+  };
+
+  const handleClear = () => {
+    dispatch(clearDisplay());
+  };
+
+  const handleMemory = (num) => {
+    dispatch(Memory(num));
+  };
 
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
+        <a className="navbar-brand" href="#">
+          <img width="40px" src="./Lambda-Logo-Red.png" /> Lambda Reducer
+          Challenge
+        </a>
       </nav>
 
-      <div className = "container row mt-5">
+      <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-            
-            <TotalDisplay value={state.total}/>
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> {state.operation} </span>
-              <span id="memory"><b>Memory:</b> {state.memory} </span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <span id="operation">
+                <b>Operation:</b> {state.operation}{" "}
+              </span>
+              <span id="memory">
+                <b>Memory:</b> {state.memory}{" "}
+              </span>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={"M+"} onClick={() => handleMemory(1)} />
+              <CalcButton value={"MR"} onClick={() => handleMemory(2)} />
+              <CalcButton value={"MC"} onClick={() => handleMemory(3)} />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={1} onClick={() => handleMath(1)} />
+              <CalcButton value={2} onClick={() => handleMath(2)} />
+              <CalcButton value={3} onClick={() => handleMath(3)} />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={4} onClick={() => handleMath(4)} />
+              <CalcButton value={5} onClick={() => handleMath(5)} />
+              <CalcButton value={6} onClick={() => handleMath(6)} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={7} onClick={() => handleMath(7)} />
+              <CalcButton value={8} onClick={() => handleMath(8)} />
+              <CalcButton value={9} onClick={() => handleMath(9)} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={"+"} onClick={() => handleOperator("+")} />
+              <CalcButton value={"*"} onClick={() => handleOperator("*")} />
+              <CalcButton value={"-"} onClick={() => handleOperator("-")} />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={() => handleClear()} />
             </div>
-
           </form>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
